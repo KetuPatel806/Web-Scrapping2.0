556c17657946dbc7ad3d6e08ddd6a9ba186d3068
KetuPatel806 <ketup806@gmail.com>
2023-03-29 09:47:02 +0530

    Web-Scrapping

--- app.py
+++ app.py
@@ -76,7 +76,8 @@ def index():
             logging.info("log my final result {}".format(reviews))
 
             
-            client = pymongo.MongoClient("mongodb+srv://pwskills:pwskills@cluster0.ln0bt5m.mongodb.net/?retryWrites=true&w=majority")
+            
+            client = pymongo.MongoClient("mongodb+srv://KetuPatel:<ketupatel806>@cluster0.wv8shqf.mongodb.net/?retryWrites=true&w=majority")
             db =client['scrapper_eng_pwskills']
             coll_pw_eng = db['scraper_pwskills_eng']
             coll_pw_eng.insert_many(reviews)
